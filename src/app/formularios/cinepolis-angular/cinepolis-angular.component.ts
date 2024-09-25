5de02ce92c4fc25002c95375562ee2005285efce
Jorge Ivan Valadez Ontiveros <vanyvanxd18@gmail.com>
2024-09-24 22:04:39 -0600

    Ejercicio Cinepolis Angular

--- src/app/formularios/cinepolis-angular/cinepolis-angular.component.ts
+++ src/app/formularios/cinepolis-angular/cinepolis-angular.component.ts
@@ -5,6 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './cinepolis-angular.component.html',
   styleUrl: './cinepolis-angular.component.css'
 })
-export class CinepolisAngularComponent {
+export class CinepolisAngularComponent
+{
+  "nombre":string
+  "personas":number
+  "boletosT":number 
+  "boletosMax":number 
+  "tarjeta":boolean
+  boletoP:number = 12
+  "total":number
+  "mensaje":string
 
-}
+  calcular() 
+  {
+    this.boletosMax = this.personas * 7;
+
+    if (this.boletosT > this.boletosMax) 
+    {
+      this.mensaje = "No puedes comprar más de: " + this.boletosMax + " boletos.";
+      return;
+    }
+
+    this.total = this.boletosT * this.boletoP;
+
+    if (this.boletosT >= 5)
+    {
+      this.total -= this.total * 0.15;
+    }
+    else 
+    {
+      this.total -= this.total * 0.10;
+    }
+
+    if (this.tarjeta)
+    {
+      this.total -= this.total * 0.10;
+    }
+
+    this.mensaje = "Calculo hecho. Total: $" + this.total.toFixed(2);
+  }
+} 
